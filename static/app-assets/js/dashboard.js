2da5db655425be8232023c32648e54de4ab16b4e
solutionLi <momo821123@outlook.com>
2020-10-21 11:19:39 +0900

    Add dashboard.html, dashboard.js

---
+++ static/app-assets/js/dashboard.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    "use strict"
+    
+    $("div[name='table_area']").each(function() {        
+        if ($(this).html() != "") {
+            var content = $(this).html()
+            content = content.replace(/&lt;/g, "<")
+            content = content.replace(/&gt;/g, ">")
+            $(this).html(content)
+            console.log("html = " + content)
+        }
+    });
+
+    $(".tab-pane").first().addClass("active")
+    $("a[name='tab_names']").first().addClass("active")
+})
